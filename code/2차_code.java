668eedd90a475afa24188ac81c8ef459950ae553
turnright170 <113104122+turnright170@users.noreply.github.com>
2026-02-23 23:35:00 +0900

    Add Main class with personal information display

---
+++ code/2차_code.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 21;
+        double height = 162.5;
+        boolean isStudent = true;
+        String name = "Seojin";
+
+        System.out.println(name + "의 나이는 " + age + "살입니다.");
+        System.out.println("키는 " + height + "cm 입니다.");
+        System.out.println("학생 여부: " + isStudent);
+    }
+}
