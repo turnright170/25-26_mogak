a2ffeb1128b65c1b3bce57d49c95694a5736aabc
turnright170 <113104122+turnright170@users.noreply.github.com>
2026-02-23 23:32:05 +0900

    Add initial Java program with main method

---
+++ code/1차_code.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java Study - Week 1");
+        System.out.println("자바는 main 메서드에서 실행이 시작된다.");
+    }
+}
